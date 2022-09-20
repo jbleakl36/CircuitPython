99a5d6c32338607002d8f255a229b95441eaf177
jbleakl36E <jbleakl36@charlottesvilleschools.org>
2022-09-20 14:31:53 -0400

    hhh

--- code.py
+++ code.py
@@ -1,5 +1,37 @@
-from time import sleep
-
+#type: ignore
+import simpleio
+import time
+import board
+import adafruit_hcsr04
+import neopixel
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D5, echo_pin=board.D4)
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+dot.brightness = 0.5 
+red =0
+blue =0
+green =0
+ 
 while True:
-    print("Hello world!")
-    sleep(40)
+
+    try:
+        print((sonar.distance, red, green, blue))
+        
+        if sonar.distance < 5:
+            red= simpleio.map_range(sonar.distance, 5, 20, 0, 255)
+            blue= 0
+            green= 0
+            dot.fill((red, 0, 0))
+        elif sonar.distance < 20 and sonar.distance> 5:
+             red= 0
+             blue= simpleio.map_range(sonar.distance, 5, 20, 0, 255)
+             green= 0
+             dot.fill((0, 0, blue))
+        else:
+             red=0
+             green= simpleio.map_range(sonar.distance, 5, 20, 0, 255)
+             blue= 0
+             dot.fill((0, green, 0))
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)
+    
