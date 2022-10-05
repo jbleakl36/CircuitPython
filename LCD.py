e2a71dba62a99edea520005a6fe6b11b150fdf77
jbleakl36E <jbleakl36@charlottesvilleschools.org>
2022-10-05 14:11:21 -0400

    Completed LCD assingment

--- LCD.py
+++ LCD.py
@@ -1,42 +1,49 @@
-#type: ignore
+#Elias Garcia
+#When a button is presses it increses or decreases the count based on the position of a switch
+#Code from Grant Gastinger
+ 
 import board
-import time
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+import time
 from digitalio import DigitalInOut, Direction, Pull
+
 # get and i2c object
 i2c = board.I2C()
+btn = DigitalInOut(board.D2)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+clickCount = 0
 
-# some LCDs are 0x3f... some are 0x27.
-lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
-
-buttonpress=0
-btn1 = DigitalInOut(board.D2)
 btn2 = DigitalInOut(board.D3)
-btn1.direction = Direction.INPUT
-btn1.pull = Pull.UP
 btn2.direction = Direction.INPUT
 btn2.pull = Pull.UP
-cur_state = True
-prev_state = True
+# some LCDs are 0x3f... some are 0x27...
+lcd = LCD(I2CPCF8574Interface(i2c, 0x3f), num_rows=2, num_cols=16)
 
-
-
+lcd.print("elias")
+print("son, i am disapoint.")
 while True:
- 
-    curr_state= btn2.value
-    
-    
-    if cur_state != prev_state:
-        if not cur_state:
-            lcd.print("BTN1 is down")
-            cur_state= False
-            prev_state= False
-          
+    if not btn2.value:
+        if not btn.value:
+            lcd.clear()
+            lcd.set_cursor_pos(0, 0)
+            lcd.print("Click Count:")
+            lcd.set_cursor_pos(0,13)
+            clickCount = clickCount + 1
+            lcd.print(str(clickCount))
         else:
-            lcd.print("BTN1 is up")
-            buttonpress = buttonpress + 1
-    prev_state = cur_state
-    time.sleep(.05)
-    
-    
+            pass
+    else:
+        if not btn.value:
+            lcd.clear()
+            lcd.set_cursor_pos(0, 0)
+            lcd.print("Click Count:")
+            lcd.set_cursor_pos(0,13)
+            clickCount = clickCount - 1
+            lcd.print(str(clickCount))
+        else:
+            pass
+    print("btn1",btn.value,"   btn2",btn2.value)
+
+    time.sleep(0.1) # sleep for debounce
